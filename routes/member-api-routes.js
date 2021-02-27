f0b3967f7f48f656438f2e9f652e3f276b69c861
jayarghargh <jayarghargh@gmail.com>
2021-02-27 13:51:23 +1030

    removed testing object from member-api-routes

--- routes/member-api-routes.js
+++ routes/member-api-routes.js
@@ -2,24 +2,12 @@
 // https://medium.com/@vivian_48490/how-to-get-full-advantage-of-handlebars-using-sequelize-express-mvc-bab17e59bceb
 const db = require('../models');
 
-const arcades = [
-	{
-		henloid: 1,
-		arcade: "joust"
-	},
-	{
-		henloid:2,
-		arcade: "frogger"
-	}
-]
-
 module.exports = (app) => {
 	app.get("/members", (req, res) => {
 		db.Members.findAll({
 		}).then((dbMembers) => {
 			res.render("index", {
 				data: dbMembers.map(member => member.toJSON()),
-				henlo: arcades
 			});
 		});
 	});
