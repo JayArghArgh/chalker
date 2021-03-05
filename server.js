b2fc25f715bab0ad20c522a9ccf99132b070b6f3
jayarghargh <jayarghargh@gmail.com>
2021-03-05 21:27:19 +1030

    added passport

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-const express = require('express');
+const express = require("express");
+const session = require("express-session");
+
+// Load in Passport
+const passport = require("./config/passport");
 
 // Add routers here
 // const membersRouter = require('./routes/member-api-routes.js')
@@ -8,19 +12,26 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Grab our models to sync.
-const db = require('./models');
+const db = require("./models");
 
 // Set up the express app to handle data parsing
-app.use(express.urlencoded( { extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static Directory
-app.use(express.static( 'public' ));
+app.use(express.static("public"));
 
 // Set Handflebars
-const exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Use sessions to keep track of our users login status.
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Invoke the routes here.
 // membersRouter(app);
@@ -31,6 +42,7 @@ require("./routes/game-api-routes.js")(app);
 
 // Sync the sequelize models and then start the express app.
 db.sequelize.sync().then(() => {
-// db.sequelize.sync({ force: true }).then(() => {
-	app.listen(PORT, () => console.log(`Chalker is running on http://localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`Chalker is running on http://localhost:${PORT}`);
+  });
 });
