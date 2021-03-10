afb675d3c1e39d17214492b153996c5a33819ccc
jayarghargh <jayarghargh@gmail.com>
2021-03-10 20:03:42 +1000

    added basic calculator modal

--- public/assets/js/burma.js
+++ public/assets/js/burma.js
@@ -4,11 +4,13 @@ console.log("loaded burma.js");
 // Add a listener for the score cell.
 
 const btnPlayerScore = document.querySelectorAll(".player-score");
+const divPlayerScore = $("#div-player-score");
 
 btnPlayerScore.forEach(button => {
   button.addEventListener("click", e => {
     e.stopPropagation();
     const id = e.target.getAttribute("data-userid");
     console.log(`clickety-clackety-this-is-whackety ${id}`);
+    divPlayerScore.html(id);
   });
 });
