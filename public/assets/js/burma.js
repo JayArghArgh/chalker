80dc29a04b7e5492e3e54f8063e5369ab4d79b2e
jayarghargh <jayarghargh@gmail.com>
2021-03-10 17:00:07 +1000

    add click event lsiteners for score

---
+++ public/assets/js/burma.js
@@ -0,0 +1,14 @@
+"use strict";
+console.log("loaded burma.js");
+
+// Add a listener for the score cell.
+
+const btnPlayerScore = document.querySelectorAll(".player-score");
+
+btnPlayerScore.forEach(button => {
+  button.addEventListener("click", e => {
+    e.stopPropagation();
+    const id = e.target.getAttribute("data-userid");
+    console.log(`clickety-clackety-this-is-whackety ${id}`);
+  });
+});
